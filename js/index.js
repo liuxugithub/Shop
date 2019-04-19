708478adec0ea2ac410acbbbe9dfeba0d3e8a148
liuxu <504741809@qq.com>
2019-04-20 00:35:51 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let listBox = document.getElementById('list'),
+    headBox = document.getElementById('head'),
+    linkList =headBox.getElementsByTagName('a'),
+    productList=listBox.getElementsByTagName('li');
+
+(function () {
+    let productData= null;
+        xhr = new XMLHttpRequest;
+
+
+
+})();
+
+catch
