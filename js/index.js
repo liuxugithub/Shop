1a66e8f28fe39b9ddd487e4a5867aa60f94e319d
liuxu <504741809@qq.com>
2019-04-20 14:15:37 +0800

    liuxu test

--- js/index.js
+++ js/index.js
@@ -1,14 +1,36 @@
-let listBox = document.getElementById('list'),
-    headBox = document.getElementById('head'),
-    linkList =headBox.getElementsByTagName('a'),
-    productList=listBox.getElementsByTagName('li');
-
 (function () {
     let productData= null;
-        xhr = new XMLHttpRequest;
+    let xhr = new XMLHttpRequest;
+    xhr.open('GET', 'json/product.json', false);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 || xhr.readyState === 200) {
+            productData = xhr.responseText;
+        }
+    };
+    xhr.send(null);
+    let str = ``;
+    let list = document.getElementById('list');
 
+    productData = JSON.parse(productData);
+    console.log(productData);
+    for (let i = 0; i < productData.length; i++) {
+        let {id, title, price, time, hot, img} = productData[i];
+        str += `<li >
+                <a href="javascript:;">
 
+                <img src="${img}">
+                    <p>"${title}"</p>
 
+                <span>${price}</span>
+                </a>
+            </li>`;
+    }
+
+    list.innerHTML = str;
 })();
 
-catch
+(function () {
+
+})
+
+
